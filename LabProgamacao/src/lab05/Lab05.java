3605c5dd2fe47e41ea07cb9139ba8a4563a2a55b
usuario <usuario@DESKTOP-8LIEST6>
2017-10-24 20:59:05 -0300

    Merge origin/master

--- LabProgamacao/src/lab05/Lab05.java
+++ LabProgamacao/src/lab05/Lab05.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lab5;
+package lab05;
 
 /**
  *
  * @author usuario
  */
-public class Lab5 {
+public class Lab05 {
     
 }

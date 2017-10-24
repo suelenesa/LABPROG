4069acc8856eb3c84d3ff703ae7d9be3d7511d7d
usuario <usuario@DESKTOP-8LIEST6>
2017-10-24 20:37:41 -0300

    Merge origin/master

---
+++ JavaApplication7/src/Helloword/HelloWord.java
@@ -0,0 +1,12 @@
+package HelloWord;
+/**
+ *
+ * @author suele
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+       System.out.println("Olá Mundo");
+    }
+          
+           
+}

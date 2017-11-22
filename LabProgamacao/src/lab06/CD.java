a8e66f768e30f72493c0ca976a86d3ab88073428
usuario <usuario@DESKTOP-8LIEST6>
2017-11-21 21:06:38 -0300

    Lab 05

---
+++ LabProgamacao/src/lab06/CD.java
@@ -0,0 +1,19 @@
+package lab06;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Suelene Sa
+ * @Herculys Sabino
+ */
+public class CD {
+    public String artista;
+    public String título ;
+    public String trilhaprincipal;
+    List<String> Mfaixas = new ArrayList();
+    
+    
+}
+
+

127d4229502bc68d4d36d105629203b179bc7ea9
Suelene Clementino de Sa <31906005+suelenesa@users.noreply.github.com>
2017-12-02 11:04:37 -0200

    Update MinhaCDteca.java

--- LabProgamacao/src/lab06/MinhaCDteca.java
+++ LabProgamacao/src/lab06/MinhaCDteca.java
@@ -1,14 +1,74 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab06;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @Suelene Sa
- * 
+ *
  */
 public class MinhaCDteca {
-    
+
+    List<CD> MeuCDs;
+
+    public MinhaCDteca() {
+        this.MeuCDs = new ArrayList<>();
+    }
+
+    public void adicionaCD(CD nome) {
+        MeuCDs.add(nome);
+
+    }
+
+    public void adicionaCDs(CD[] nome) {
+        MeuCDs.addAll(Arrays.asList(nome));
+    }
+
+    public CD removeCD(CD nome) {
+        for (int i = 0; i < MeuCDs.size(); i++) {
+            if (nome.equals(MeuCDs.get(i).getTítulo())) {
+                MeuCDs.remove(nome);
+                continue;
+            }
+            return nome;
+
+        }
+        return null;
+    }
+
+    public boolean removeCDs(CD[] nome) {
+        int cont = 0;
+        for (int i = 0; i < MeuCDs.size(); i++) {
+            if (nome.equals(MeuCDs.get(i).getTítulo())) {
+                MeuCDs.remove(nome);
+                cont++;
+            }
+        }
+        return cont >= 1;
+    }
+
+    public CD pesquisaCD(CD nome) {
+        for (int i = 0; i < MeuCDs.size(); i++) {
+            if (nome.equals(MeuCDs.get(i).getTítulo())) {
+                // return nome;
+            } else {
+                return null;
+            }
+        }
+        return nome;
+    }
+
+    public void numeroDeCDs() {
+        System.out.println(MeuCDs.size());
+    }
+
+    @Override
+    public String toString() {
+        for (int i = 0; i < MeuCDs.size(); i++) {
+            System.out.println(MeuCDs.get(i));
+        }
+        return super.toString();
+    }
+
 }

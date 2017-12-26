7da2a2d3903e28929f7506ae323f5ac1e173ab0b
Suelene Clementino de Sa <31906005+suelenesa@users.noreply.github.com>
2017-12-26 00:23:00 -0200

    Add files via upload

---
+++ LabProgamacao/src/Usuario.java
@@ -0,0 +1,41 @@
+
+public class Usuario {
+	
+	private int notas;
+	private String comentarios;
+	private int data;
+	private int notaNm;
+	
+	
+	public  Usuario(){
+		notas = this.notas;
+		comentarios = this.comentarios;
+		
+	}
+		public void opinarNota(int notas ){
+			
+
+			if (notas == -2 || notas == -1 || notas == 0 || notas == 1 || notas == 2){
+				System.out.println("Você opinou " + notas);
+			}
+			
+			else {
+				System.out.println("Você nao pode opinar com essa nota");
+			}
+			
+			
+		}
+		
+		public void comentarAgora(String comentarios){
+			if (comentarios.length() > 140 ){
+				System.out.println("Você nao pode comentar, caracteres ultrapassaram");
+			}
+		}
+		
+		public void notaNamosca(){
+			
+		}
+	
+	
+
+}

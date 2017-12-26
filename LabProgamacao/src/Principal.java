923b98a14622fa7dc7c2b2a902820a32889b61ec
Suelene Clementino de Sa <31906005+suelenesa@users.noreply.github.com>
2017-12-26 00:21:45 -0200

    Add files via upload

---
+++ LabProgamacao/src/Principal.java
@@ -0,0 +1,14 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Usuario u = new Usuario();
+		
+		u.opinarNota(0);
+		u.comentarAgora("Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem seddddndo utilizado desde o século XVI, quando");
+		// TODO Auto-generated method stub
+
+	}
+
+}
